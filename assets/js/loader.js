121a57993c166355b29d123dc8a4d2c74762b11d
Tony Yip <tony@opensource.hk>
2016-11-27 17:34:59 +0800

    Change replace regex to prevent replace of other title

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -8,15 +8,18 @@ module.exports = function (url, component) {
       return response.text();
     })
     .then((content) => {
-      const title = content.match(/###(.+)/);
-      component.content = marked(content.replace(/^###.+/, ''));
+      const title = content.match(/###([^#]+)/);
+      component.content = marked(content.replace(/^###[^#]+/, ''));
       component.title = title && title.length > 1 ? title[1].trim() : '';
       component.done = true;
     })
     .catch((e) => {
       console.error(e.stack);
       component.title = 'Opps! Error occur!';
-      component.content = 'Please check your network or any typing missing in the website. If you confirm no this two, please report it at <a href="https://github.com/spyc/library/issues/new">Github</a> or email to tony@opensource.hk';
+      component.content = 'Please check your network or any typing missing in the website.' +
+        ' If you confirm no this two,' +
+        'please report it on <a href="https://github.com/spyc/library/issues/new">Github</a>' +
+        'or email to tony@opensource.hk';
       component.done = true;
     });
 };
