385fb1dfdd7cf01bc6cbee2beb4cc9fec7334325
Tony Yip <tony@opensource.hk>
2016-11-29 01:43:05 +0800

    Replace with markdown card

--- assets/js/page/Book.vue
+++ assets/js/page/Book.vue
@@ -5,13 +5,7 @@
             <container>
                 <row>
                     <div class="col s12">
-                        <div class="card">
-                            <div class="card-content">
-                                <loader v-if="!done" />
-                                <div class="card-title" v-if="done">{{ title }}</div>
-                                <div v-html="content" v-if="done"></div>
-                            </div>
-                        </div>
+                        <markdown-card :url="url" />
                     </div>
                 </row>
             </container>
@@ -19,24 +13,17 @@
     </div>
 </template>
 <script>
-  import loader from '../loader';
   export default{
     data(){
       return {
-        done: false,
-        content: '',
-        title: ''
+        url: `books/${this.$route.params.id}.txt`
       }
     },
     components: {
       Header: require('../component/Header.vue'),
       container: require('../component/Container.vue'),
       row: require('../component/Row.vue'),
-      loader: require('../component/Loader.vue')
-    },
-    mounted() {
-      const url = `https://spyc.github.io/library-data/books/${this.$route.params.id}.txt`;
-      loader(url, this);
+      'markdown-card': require('../component/MarkdownCard.vue')
     }
   }
 </script>
