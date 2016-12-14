95e815fd707ef0055ffa7c908e39a0b093448526
Tony Yip <tony@opensource.hk>
2016-12-15 01:18:40 +0800

    Apply for code split

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,27 @@
 'use strict';
 
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
-
 if (location.hash.indexOf('%21') > -1) {
   location.href = location.href.replace('%21', '');
 }
+require.ensure(['vue', 'vue-router', 'vue-resource'], function () {
+  const Vue = require('vue');
+  const VueRouter = require('vue-router');
+  const VueResource = require('vue-resource');
+  Vue.use(VueResource);
 
-Vue.use(VueResource);
+  const router = new VueRouter(require('./route'));
 
-const router = new VueRouter(require('./route'));
+  router.beforeEach((to, from, next) => {
+    const sidenav = document.querySelector('[data-sidenav]');
+    if (sidenav) sidenav.parentNode.removeChild(sidenav);
+    next();
+  });
 
-router.beforeEach((to, from, next) => {
-  const sidenav = document.querySelector('[data-sidenav]');
-  if (sidenav) sidenav.parentNode.removeChild(sidenav);
-  next();
+  new Vue({
+    router,
+    mounted() {
+      const loader = document.getElementById('page-loader');
+      loader.parentNode.removeChild(loader);
+    }
+  }).$mount('#app');
 });
-
-new Vue({
-  router,
-  mounted() {
-    const loader = document.getElementById('page-loader');
-    loader.parentNode.removeChild(loader);
-  }
-}).$mount('#app');
--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,31 +1,31 @@
 'use strict';
-
-import marked from 'marked';
-
-module.exports = function (url, component) {
-  component.$http.get(url)
-    .then((response) => {
-      return response.text();
-    })
-    .then((content) => {
-      const titleRegexp = /###\s+(.+)/;
-      const imageRegexp = /!\[([^\]]+)]\(([^)]+)\)/;
-      const title = content.match(titleRegexp);
-      const image = content.match(imageRegexp);
-      const text = content.replace(titleRegexp, '').replace(imageRegexp, '');
-      component.content = marked(text);
-      component.title = title && title.length > 1 ? title[1].trim() : '';
-      component.done = true;
-      component.image = image && image.length > 1 ? image[2].trim() : false;
-      component.key = image && image.length > 1 ? image[1].trim() : false;
-    })
-    .catch((e) => {
-      console.error(e.stack);
-      component.title = 'Opps! Error occur!';
-      component.content = 'Please check your network or any typing missing in the website.' +
-        ' If you confirm no this two,' +
-        'please report it on <a href="https://github.com/spyc/library/issues/new">Github</a> ' +
-        'or email to tony@opensource.hk';
-      component.done = true;
-    });
-};
+require.ensure(['marked'], function () {
+  const marked = require('marked');
+  module.exports = function (url, component) {
+    component.$http.get(url)
+      .then((response) => {
+        return response.text();
+      })
+      .then((content) => {
+        const titleRegexp = /###\s+(.+)/;
+        const imageRegexp = /!\[([^\]]+)]\(([^)]+)\)/;
+        const title = content.match(titleRegexp);
+        const image = content.match(imageRegexp);
+        const text = content.replace(titleRegexp, '').replace(imageRegexp, '');
+        component.content = marked(text);
+        component.title = title && title.length > 1 ? title[1].trim() : '';
+        component.done = true;
+        component.image = image && image.length > 1 ? image[2].trim() : false;
+        component.key = image && image.length > 1 ? image[1].trim() : false;
+      })
+      .catch((e) => {
+        console.error(e.stack);
+        component.title = 'Opps! Error occur!';
+        component.content = 'Please check your network or any typing missing in the website.' +
+          ' If you confirm no this two,' +
+          'please report it on <a href="https://github.com/spyc/library/issues/new">Github</a> ' +
+          'or email to tony@opensource.hk';
+        component.done = true;
+      });
+  };
+});
